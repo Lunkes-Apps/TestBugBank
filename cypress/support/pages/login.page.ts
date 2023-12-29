18c667d313a51773b20988bea87e00cf07e8fdd0
Alexandre Lunkes <lunkes.alexandre@gmail.com>
2023-12-29 19:56:31 -0300

    ajustes

--- cypress/support/pages/login.page.ts
+++ cypress/support/pages/login.page.ts
@@ -15,13 +15,39 @@ export class LoginPage {
     passwordRegister: string = '.card__register [name="password"]'
     passworConfirmRegister: string = '.card__register [name="passwordConfirmation"]'
 
+    toggleBalance: string = '#toggleAddBalance'
+
+
     login = (email: string, password: string): LoginPage => {
         cy.get(this.emailLogin)
         .type(email)
         .get(this.passwordLogin)
         .type(password)
-        .
+        .get(this.loginButton).click()
         return this;
+    }
+
+    register(email: string, name: string, 
+             password: string, passwordConfirm: string,
+             withBalance: string): LoginPage{
+        cy.get(this.emailLoginRegister)
+        .type(email)
+        .get(this.nameRegister)
+        .type(name)
+        .get(this.passwordRegister)
+        .type(password)
+        .get(this.passworConfirmRegister)
+        .type(passwordConfirm)
+
+        if()
+
+    }
+
+    isToggleTrue = (): boolean =>{
+        cy.get(this.toggleBalance).parent().then(($element) =>{
+            $element.
+            return true;
+        })
     }
 
 }
